5095c967623497d57a373cf277682dc17d5caeb6
Usama446 <us273852@gmail.com>
2025-03-19 12:51:10 -0400

    new Commit

--- src/app/Data/source_file.ts
+++ src/app/Data/source_file.ts
@@ -172,6 +172,16 @@ export const natureBlog = {
             image: "assets/images/winter/winter4.jpg", 
             flag: "Adventure" 
         },  
+        { 
+            id: 4005, 
+            postType: "Winter", 
+            heading: "Winter's Golden Glow Usama",
+            subheading: "A breathtaking sunset over a snowy wonderland",
+            description: "The sun sets behind snow-covered trees, casting a golden glow over the icy landscape",
+            long_description: "As the sun dips below the horizon, its golden rays illuminate the snow-covered forest, creating a mesmerizing contrast of warmth and cold. The untouched snow glistens under the soft light, reflecting hues of orange, pink, and blue. Each tree stands as a frosty masterpiece, its branches weighed down by layers of pure white snow. The distant mountains add to the serene beauty of the scene, stretching into the horizon like a frozen paradise. A crisp, refreshing chill fills the air, enhancing the peaceful ambiance of winter's embrace. This is nature’s perfect harmony, where the stillness of snow meets the brilliance of the setting sun",
+            image: "assets/images/winter/winter3.jpg", 
+            flag: "Food & Drinks" 
+        },
        
     ]
 };
